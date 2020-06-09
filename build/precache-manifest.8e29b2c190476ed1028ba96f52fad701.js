0ca7b39fb8f6d8ef29f15f1e97df27c2243f2f60
Alexej <alexejdubow@gmail.com>
2020-06-09 18:36:17 +0300

    delete

---
+++ build/precache-manifest.8e29b2c190476ed1028ba96f52fad701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de7888dccc9f2dc33e823f8a999f7d7e",
+    "url": "/hw-03-image-finder-ver-02/index.html"
+  },
+  {
+    "revision": "696066c6202e544e50fd",
+    "url": "/hw-03-image-finder-ver-02/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc2dcf2d328c59788a4b",
+    "url": "/hw-03-image-finder-ver-02/static/js/2.887cec14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw-03-image-finder-ver-02/static/js/2.887cec14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "696066c6202e544e50fd",
+    "url": "/hw-03-image-finder-ver-02/static/js/main.096dd423.chunk.js"
+  },
+  {
+    "revision": "6ed41689ed82b93b9359",
+    "url": "/hw-03-image-finder-ver-02/static/js/runtime-main.236b45e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hw-03-image-finder-ver-02/static/media/logo.5d5d9eef.svg"
+  }
+]);
